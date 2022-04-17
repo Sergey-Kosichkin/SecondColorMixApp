6beb8b369b9a4e2c54f859c67bb757e8f956c51b
Sergey-Kosichkin <68821779+Sergey-Kosichkin@users.noreply.github.com>
2022-04-17 11:36:30 +0300

    Added logic of sliders and main view to change color.

--- SecondColorMixApp/ViewController.swift
+++ SecondColorMixApp/ViewController.swift
@@ -9,11 +9,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var redValueLabel: UILabel!
+    @IBOutlet var greenValueLabel: UILabel!
+    @IBOutlet var blueValueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        colorView.layer.cornerRadius = 10
+        
+        redSliderAction()
+        greenSliderAction()
+        blueSliderAction()
     }
 
-
+    @IBAction func redSliderAction() {
+        redValueLabel.text = roundToString(redSlider)
+        changeColor()
+    }
+    
+    @IBAction func greenSliderAction() {
+        greenValueLabel.text = roundToString(greenSlider)
+        changeColor()
+    }
+    
+    @IBAction func blueSliderAction() {
+        blueValueLabel.text = roundToString(blueSlider)
+        changeColor()
+    }
+    
+    private func roundToString(_ slider: UISlider) -> String {
+        return String(format: "%.2f", slider.value)
+    }
+    
+    private func changeColor() {
+        colorView.backgroundColor = UIColor(red: CGFloat(redSlider.value),
+                                            green: CGFloat(greenSlider.value),
+                                            blue: CGFloat(blueSlider.value),
+                                            alpha: 1)
+    }
 }
 
